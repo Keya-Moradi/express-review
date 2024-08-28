f2499524ed83d77811b023a80d4fd8823967721d
keya-moradi <keya.moradi@gmail.com>
2024-08-27 19:36:42 -0700

    update todo routes

--- server.js
+++ server.js
@@ -1,28 +1,47 @@
 // DEPENDENCIES
+///////////////////
 const express = require('express')
 const morgan = require('morgan')
 
-const app = express()
+const app = express();
 const { PORT = 3000 } = process.env
 
 const todos = [
     {
-        title: "My first todo",
+        title: "My first Todo",
         content: "My first message"
     }
 ]
 
 // MIDDLEWARE
+///////////////////
 app.use(morgan('dev'))
-
+app.use(express.urlencoded({ extended: false })) // gives us access to req.body from x-http-url-form
 
 // ROUTES
+///////////////////
 app.get('/', (req, res) => {
-    res.send('Honey Im home')
+    res.send('Honey im home')
 })
 
-// INDeX
+// INDEX 
 app.get('/todos', (req, res) => {
+    res.json(todos)
+})
+
+// CREATE
+app.post('/todos', (req, res) => {
+
+    // create our new todo from the request body
+    let newTodo = {
+        title: req.body.title,
+        content: req.body.content
+    }
+
+    // append our new todo to the todos array
+    todos.push(newTodo)
+
+    // then show the new todos
     res.json(todos)
 })
 
@@ -30,18 +49,12 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
     let id = parseInt(req.params.id);
 
-    if (NaN(Number(req.params.id))) {
-    }
-    // find out todo based on the request param
-    let foundTodo = todos[req.params.id]
+    // find our todo based on the request param
+    let foundTodo = todos[id]
     // respond with the found todo
+    res.json(foundTodo)
 })
 
-
 // LISTENER
-app.listen(PORT, () => console.log('Listening on port: ${PORT}'))
-
-
-
-
-
+///////////////////
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
