f947b1a503aa07debee81aa82aacf0bb5cd34d5f
keya-moradi <keya.moradi@gmail.com>
2024-08-27 19:27:16 -0700

    setup GitHub

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const morgan = require('morgan')
 const app = express()
 const { PORT = 3000 } = process.env
 
+const todos = [
+    {
+        title: "My first todo",
+        content: "My first message"
+    }
+]
+
 // MIDDLEWARE
 app.use(morgan('dev'))
 
@@ -12,6 +19,22 @@ app.use(morgan('dev'))
 // ROUTES
 app.get('/', (req, res) => {
     res.send('Honey Im home')
+})
+
+// INDeX
+app.get('/todos', (req, res) => {
+    res.json(todos)
+})
+
+// SHOW
+app.get('/todos/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+
+    if (NaN(Number(req.params.id))) {
+    }
+    // find out todo based on the request param
+    let foundTodo = todos[req.params.id]
+    // respond with the found todo
 })
 
 
